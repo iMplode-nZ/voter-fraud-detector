3323952c972a8b2aa2538ee03fdf2d1fcceecc61
iMplode nZ <rg@youxplode.com>
2020-11-06 18:01:44 -0800

    Finished.

--- index.js
+++ index.js
@@ -1,21 +1,82 @@
 'use strict';
 
-const input = document.createElement('input');
-input.type = 'file';
-input.style.display = 'none';
-document.body.appendChild(input);
-input.click();
-input.onchange = () => {
+document.getElementById('button').onclick = click;
+// Create a worker from worker.js which is loaded using a script tag.
+const worker = new Worker(
+    URL.createObjectURL(
+        new Blob(['(' + workerFunction.toString() + ')()'], { type: 'text/javascript' })
+    )
+);
+const preInfo = document.getElementById('pre-info');
+const info = document.getElementById('info');
+// This appends text to the parent.
+function text(parent, text) {
+    const div = document.createElement('div');
+    div.innerText = text;
+    parent.appendChild(div);
+}
+// Append a table row.
+function table(parent, rowData, tableType) {
+    const row = document.createElement('tr');
+    rowData.forEach((x) => {
+        const cell = document.createElement(tableType);
+        cell.innerText = x;
+        row.appendChild(cell);
+    })
+    parent.appendChild(row);
+}
+// Appends a pre.
+function pre(parent, data) {
+    const pre = document.createElement('pre');
+    pre.innerHTML = data;
+    parent.appendChild(pre);
+}
+
+function click() {
+    console.log('Clicked');
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.style.display = 'none';
+    document.body.appendChild(input);
+    input.click();
+    window.input = input;
+    input.onchange = loadFile;
+}
+
+function loadFile() {
     console.log('Loading File...');
     const file = input.files[0];
     if (! file.name.endsWith('.csv')) {
         throw new Error('Invalid File. Only `.csv` files are supported.');
     }
-    const reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = (e) => {
-        console.log(e);
-        const voters = reader.result;
-        window.voters = voters;
+    worker.postMessage([file, {
+        tolerance: 100, // This number is in years, and represents how old a voter could be before it becomes suspicious.
+        /* Settings to be filled in when necessary */
+    }]);
+    text(preInfo, 'Loading...');
+}
+
+worker.onmessage = (msg) => {
+    const data = msg.data;
+    switch (data.shift()) {
+        case 'finished':
+            text(preInfo, 'Finished loading.');
+            break;
+        case 'header':
+            table(info, data, 'th');
+            break;
+        case 'out_of_tolerance':
+            table(info, data, 'td');
+            break;
+        case 'finished_parsing':
+            text(preInfo, 'Finished parsing.');
+            // These are all the votes that are suspicious, mapped by party.
+            text(preInfo, 'Vote Breakdown by Party.');
+            pre(preInfo, JSON.stringify(data[0], null, 4));
+            text(preInfo, 'Vote Breakdown by Party; Voters over age of 100.');
+            pre(preInfo, JSON.stringify(data[1], null, 4));
+            text(preInfo, 'Years of registration and number of voters over age of 100 which registered that year.');
+            pre(preInfo, JSON.stringify(data[2], null, 4));
+            break;
     }
 }
---
+++ worker.js
@@ -0,0 +1,97 @@
+// Due to me testing this using the local filesystem, I have to do this hack.
+
+function workerFunction() {
+    self.onmessage = (msg) => {
+        const reader = new FileReader();
+        const settings = msg.data[1];
+        reader.readAsText(msg.data[0]);
+        reader.onload = () => {
+            const voters = reader.result;
+            parse(voters, settings);
+        }
+    }
+    // Parse the voter data.
+    // This makes sure to only load each line at once to avoid using too much memory.
+    function parse(voters, settings) {
+        console.log('Finished');
+        self.postMessage(['finished']);
+        const regex = /(.*?)(\n|,)/y;
+        function readLine() {
+            const res = [];
+            let out;
+            do {
+                out = regex.exec(voters);
+                res.push(out[1]);
+            } while (out[2] !== '\n');
+            return res;
+        }
+        const headers = readLine();
+        const dateOfBirthIndex = headers.indexOf('Date of Birth');
+        const partyIndex = headers.indexOf('Applicant Party Designation');
+        const registerYearIndex = headers.indexOf('Application Approved Date');
+        headers.unshift('header');
+        self.postMessage(headers);
+        const partyMapping = {
+            'D': 'Democrat',
+            'R': 'Republican',
+            'O': 'Other',
+        }
+        const partyUnusualTotals = {
+            'D': 0,
+            'R': 0,
+            'O': 0,
+        }
+        const partyTotals = {
+            'D': 0,
+            'R': 0,
+            'O': 0,
+        }
+        const ELECTION_YEAR = 2020;
+        const yearOfRegistration = {};
+        // This adds one to the totals sorted by party counter that is passed in as obj (with party name).
+        function addTotal(obj, name) {
+            if (!(name in obj)) {
+                obj['O']++;
+            } else {
+                obj[name]++;
+            }
+        }
+        try {
+            while (true) {
+                const line = readLine();
+                const yearDifference = ELECTION_YEAR - new Date(line[dateOfBirthIndex]).getFullYear();
+                if (yearDifference > settings.tolerance) {
+                    addTotal(partyUnusualTotals, line[partyIndex]);
+                    const registerDate = new Date(line[registerYearIndex]).getFullYear();
+                    if (!yearOfRegistration[registerDate]) {
+                        yearOfRegistration[registerDate] = 0;
+                    }
+                    yearOfRegistration[registerDate]++;
+                    line.unshift('out_of_tolerance');
+                    self.postMessage(line);
+                }
+                addTotal(partyTotals, line[partyIndex]);
+            }
+        } catch(e) {
+            if (!e.message.includes(`Cannot read property '1' of null`)) {
+                console.log(e);
+            }
+            // This Error is expected to be:
+            // | Uncaught TypeError: Cannot read property '1' of null.
+            // This optimization reduces the amount of if statements within readLine by one.
+        }
+        function addNamesToTotals(totals) {
+            // Obvious code.
+            return Object.entries(totals).map(x => [partyMapping[x[0]], x[1]]).reduce((result, current) => {
+                result[current[0]] = current[1];
+                return result;
+            }, {})
+        }
+        self.postMessage([
+            'finished_parsing',
+            addNamesToTotals(partyTotals),
+            addNamesToTotals(partyUnusualTotals),
+            yearOfRegistration,
+        ]);
+    }
+}
